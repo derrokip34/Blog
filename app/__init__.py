d91dccdb495d16ad5a9810c821d3fdae0774049c
derrickip34 <derrickip34@gmail.com>
2020-05-09 12:30:03 +0300

    register auth_blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from config import config_options
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -14,5 +19,9 @@ def create_app(config_name):
 
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
     return app
